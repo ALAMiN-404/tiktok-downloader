584881c6a13d04dff30abea206e731eecd86d3e7
ALAMiN <100506616+ALAMiN-404@users.noreply.github.com>
2025-07-31 17:16:13 +0600

    Create ads.js

---
+++ static/ads.js
@@ -0,0 +1,9 @@
+function showPopupAd() {
+    const popupAd = document.getElementById("popupAd");
+    popupAd.style.display = "block";
+}
+
+function closePopupAd() {
+    const popupAd = document.getElementById("popupAd");
+    popupAd.style.display = "none";
+}
